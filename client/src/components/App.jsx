f2e63d6acb9b417ee8447a969f9c2ba7aba54bd3
Nick Anich <infinitevolume@gmail.com>
2021-06-30 20:58:54 -0700

    Added GroceryList component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import groceriesData from '../data/groceriesData.js'
+import GroceryList from './GroceryList.jsx';
+
 const App = () => (
   <div>
     <img src="grocery-bags.png" />
@@ -9,25 +12,51 @@ const App = () => (
       <label> Item
         <input name="item" value="" />
       </label>
+      {/* <Item /> */}
       <label> Quantity
         <input name="quantity" value="" />
       </label>
+      {/* <Quantity /> */}
       <button>Add Grocery</button>
       <ul className="groceries">
-        <li>
-          <span>apples - </span>
-          <span>4</span>
-        </li>
-        <li>
-          <span>bread - </span>
-          <span>2</span>
-        </li>
-        <li>
-          <span>etc...</span>
-        </li>
+        <GroceryList />
       </ul>
     </form>
   </div>
 );
 
 export default App;
+
+
+// *** ORIGINGAL CODE ***
+
+// const App = () => (
+//   <div>
+//     <img src="grocery-bags.png" />
+//     <h1>Grocery List</h1>
+//     <form>
+//       <label> Item
+//         <input name="item" value="" />
+//       </label>
+//       <label> Quantity
+//         <input name="quantity" value="" />
+//       </label>
+//       <button>Add Grocery</button>
+//       <ul className="groceries">
+//         <li>
+//           <span>apples - </span>
+//           <span>4</span>
+//         </li>
+//         <li>
+//           <span>bread - </span>
+//           <span>2</span>
+//         </li>
+//         <li>
+//           <span>etc...</span>
+//         </li>
+//       </ul>
+//     </form>
+//   </div>
+// );
+
+// export default App;
---
+++ client/src/components/GroceryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GroceryList = (props) => (
+  <ul>
+    <li>milk</li>
+    <li>cheese</li>
+    <li>other</li>
+  </ul>
+)
+
+export default GroceryList
